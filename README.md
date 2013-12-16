16697e20df77a035b9581e9cc4b1ddbd7c9c168c
John Madhavan-Reese <jsmreese@pureshare.com>
2013-12-16 11:46:47 -0500

    Initial project commit.

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
-moment-duration-format
-======================
+#moment-duration-format
 
-Format function plugin for the Moment Duration object.
+##Format function plugin for the Moment Duration object.
+
+This is a plugin to the Moment.js JavaScript date library to add comprehensive formatting to Moment Durations.
+
+Format template grammar is patterned on the existing Moment Date format template grammar, with a few modifications because durations have different needs than dates.
+
+The format function may be called with three optional arguments:
+```
+moment.duration.format([template] [, precision] [, settings])
+```
+
+Both the `template` and `precision` arguments may be specified as part of a single `settings` argument if desired.
+
+Within the `template` string, moment-token characters will be replaced with the duration value for that type.
+Moment-tokens maybe be customized (see test cases for examples), but the default token map is:
+```
+years: Y or y
+months: M
+weeks: W or w
+days: D or d
+hours: H or h
+minutes: m
+seconds: s
+milliseconds: S
+```
+
+
+Basic usage:
+```
+moment.duration(123, "minutes").format();
+// "2:03:00"
+
+moment.duration(123, "minutes").format("h:mm");
+// "2:03"
+
+moment.duration(123, "minutes").format("h [hrs], m [min]");
+// "2 hrs, 3 min"
+// escape moment-tokens using square brackets
+// this can be customized using `settings.escape`
+
+moment.duration(123, "minutes").format("h [hrs]", 2);
+// "2.04 hrs"
+// show arbitrary decimal precision with positive precision
+
+moment.duration(123, "minutes").format("m [min]", -1);
+// "120 min"
+// truncate the final value with negative precision
+
+moment.duration(123, "minutes").format("d[d] h:mm:ss");
+// "2:03:00"
+// automatically trim leading tokens that have no value
+
+moment.duration(123, "minutes").format("[seconds:] s -- [minutes:] m -- [hours:] h -- [days:] d", { trim: "right" });
+// "seconds: 0 -- minutes: 3 -- hours: 2"
+// or trim from the right
+
+moment.duration(123, "minutes").format("d[d] h:mm:ss", { trim: false });
+// "0d 2:03:00"
+// or don't trim at all
+```
---
+++ test/moment-duration-format-tests.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+	module("Moment Duration Format");
+
+	test("Basic Use", function () {
+		equal(moment.duration(1, "milliseconds").format("S"), "1");
+		equal(moment.duration(1, "seconds").format("s"), "1");
+		equal(moment.duration(1, "minutes").format("m"), "1");
+		equal(moment.duration(1, "hours").format("h"), "1");
+		equal(moment.duration(1, "hours").format("H"), "1");
+		equal(moment.duration(1, "days").format("d"), "1");
+		equal(moment.duration(1, "days").format("D"), "1");
+		equal(moment.duration(1, "weeks").format("w"), "1");
+		equal(moment.duration(1, "weeks").format("W"), "1");
+		equal(moment.duration(1, "months").format("M"), "1");
+		equal(moment.duration(1, "years").format("y"), "1");
+		equal(moment.duration(1, "years").format("Y"), "1");
+	});
+
+	test("Trim Left", function () {
+		equal(moment.duration(1, "seconds").format("m s"), "1");
+		equal(moment.duration(1, "minutes").format("h m s"), "1 0");
+	});
+
+	test("Trim Right", function () {
+		equal(moment.duration(1, "seconds").format("s m", { trim: "right" }), "1");
+		equal(moment.duration(1, "minutes").format("s m h", { trim: "right" }), "0 1");
+	});
+
+	test("Trim False", function () {
+		equal(moment.duration(1, "seconds").format("m s", { trim: false }), "0 1");
+		equal(moment.duration(1, "minutes").format("h m s", { trim: false }), "0 1 0");
+	});
+
+	test("Token Length", function () {
+		equal(moment.duration(1, "seconds").format("ss"), "01");
+		equal(moment.duration(1, "minutes").format("mm ss"), "01 00");
+	});
+
+	test("Left Trimmed First Token Length", function () {
+		equal(moment.duration(1, "seconds").format("mm ss"), "01");
+		equal(moment.duration(1, "seconds").format("m ss"), "1");
+		equal(moment.duration(1, "seconds").format("m ss", { forceLength: true }), "01");
+	});
+
+	test("Right Trimmed First Token Length", function () {
+		equal(moment.duration(1, "seconds").format("ss mm", { trim: "right" }), "01");
+		equal(moment.duration(1, "seconds").format("ss m", { trim: "right" }), "1");
+		equal(moment.duration(1, "seconds").format("ss m", { trim: "right", forceLength: true }), "01");
+	});
+
+	test("Positive Precision", function () {
+		equal(moment.duration(15, "seconds").format("m", 2), "0.25");
+		equal(moment.duration(20, "seconds").format("m", 3), "0.333");
+		equal(moment.duration(30, "seconds").format("m", 4), "0.5000");
+		equal(moment.duration(40, "seconds").format("m", 5), "0.66666");
+		equal(moment.duration(45, "seconds").format("m", 1), "0.7");
+		equal(moment.duration(59, "seconds").format("m", 0), "0");
+		equal(moment.duration(59, "seconds").format("m"), "0");
+	});
+
+	test("Negative Precision", function () {
+		equal(moment.duration(123, "seconds").format("s", -2), "100");
+	});
+
+	test("Multiple Token Instances", function () {
+		equal(moment.duration(123, "seconds").format("s s s"), "123 123 123");
+		equal(moment.duration(123, "seconds").format("s s ssssss"), "123 123 000123");
+	});
+
+	test("Escape Tokens", function () {
+		equal(moment.duration(123, "seconds").format("[All] [tokens] [escaped]"), "All tokens escaped");
+		equal(moment.duration(123, "seconds").format("s[s]"), "123s");
+	});
+
+	test("All Moment Tokens", function () {
+		// obviously a duration of 100,000,000,013 ms will vary in the number of days based on leap years, etc.
+		// this test ensures the internal duration/format math remains consistent
+		equal(moment.duration(100000000013, "ms").format("y[y] M[mo] w[w] d[d] h[h] m[m] s[s] S[ms]"), "3y 2mo 0w 2d 9h 46m 40s 13ms");
+	});
+
+	test("Output To Lesser Units", function () {
+		equal(moment.duration(1, "years").format("y"), "1");
+		equal(moment.duration(1, "years").format("M"), "12");
+		equal(moment.duration(1, "years").format("w"), "52");
+		equal(moment.duration(1, "years").format("d"), "365");
+		equal(moment.duration(1, "years").format("h"), "8760");
+		equal(moment.duration(1, "years").format("m"), "525600");
+		equal(moment.duration(1, "years").format("s"), "31536000");
+		equal(moment.duration(1, "years").format("S"), "31536000000");
+	});
+
+	test("Output To Greater Units", function () {
+		equal(moment.duration(1, "milliseconds").format("y", 15), "0.000000000001709");
+		equal(moment.duration(1, "milliseconds").format("M", 13), "0.0000000000858");
+		equal(moment.duration(1, "milliseconds").format("w", 12), "0.000000000653");
+		equal(moment.duration(1, "milliseconds").format("d", 11), "0.00000000157");
+		equal(moment.duration(1, "milliseconds").format("h", 10), "0.0000000777");
+		equal(moment.duration(1, "milliseconds").format("m", 7), "0.0000166");
+		equal(moment.duration(1, "milliseconds").format("s", 3), "0.001");
+		equal(moment.duration(1, "milliseconds").format("S"), "1");
+	});
+
+	test("Custom Token Types List", function () {
+		equal(moment.duration(12345, "seconds").format("d [days] m [minutes] h [(hours is not a token type now)]", {
+			types: "escape years months weeks days minutes seconds milliseconds general"
+		}), "205 minutes h (hours is not a token type now)");
+	});
+
+	test("Custom Escape RegExp", function () {
+		equal(moment.duration(1234, "hours").format("d (days), h (hours)", {
+			escape: /\((.+?)\)/
+		}), "51 days, 10 hours");
+	});
+
+	test("Custom Moment Token RegExp", function () {
+		equal(moment.duration(1234, "days").format("x yrs", {
+			years: /x+/,
+			seconds: /z+/,
+			precision: 2
+		}), "3.38 yrs");
+	});
+	
+	test("Using Only Settings Argument", function () {
+		equal(moment.duration(1234.55, "hours").format({
+			template: "d (days), h (hours)",
+			escape: /\((.+?)\)/,
+			precision: 1
+		}), "51 days, 10.5 hours");
+	});
+});
