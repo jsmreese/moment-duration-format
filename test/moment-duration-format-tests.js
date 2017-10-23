ed0a214f9586d5544eb933041ec19da63e3197d4
John Sharp Madhavan-Reese <john.madhavan-reese@youi.tv>
2017-10-22 21:18:24 -0400

    Fix tests from month/day changes in moment.

--- test/moment-duration-format-tests.js
+++ test/moment-duration-format-tests.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
 		equal(moment.duration(15, "seconds").format("s", -1, { trunc: true }), "10");
 		equal(moment.duration(159, "seconds").format("s", -1, { trunc: true }), "150");
 	});
-	
+
 	test("Multiple Token Instances", function () {
 		equal(moment.duration(123, "seconds").format("s s s"), "123 123 123");
 		equal(moment.duration(123, "seconds").format("s s ssssss"), "123 123 000123");
@@ -89,7 +89,7 @@ $(document).ready(function() {
 	test("All Moment Tokens", function () {
 		// obviously a duration of 100,000,000,013 ms will vary in the number of days based on leap years, etc.
 		// this test ensures the internal duration/format math remains consistent
-		equal(moment.duration(100000000013, "ms").format("y[y] M[mo] w[w] d[d] h[h] m[m] s[s] S[ms]"), "3y 2mo 0w 2d 9h 46m 40s 13ms");
+		equal(moment.duration(100000000013, "ms").format("y[y] M[mo] w[w] d[d] h[h] m[m] s[s] S[ms]"), "3y 2mo 0w 0d 9h 46m 40s 13ms");
 	});
 
 	test("Output To Lesser Units", function () {
@@ -105,7 +105,7 @@ $(document).ready(function() {
 
 	test("Output To Greater Units", function () {
 		equal(moment.duration(1, "milliseconds").format("y", 13), "0.0000000000317");
-		equal(moment.duration(1, "milliseconds").format("M", 12), "0.000000000386");
+		equal(moment.duration(1, "milliseconds").format("M", 12), "0.000000000380");
 		equal(moment.duration(1, "milliseconds").format("w", 11), "0.00000000164");
 		equal(moment.duration(1, "milliseconds").format("d", 10), "0.0000000116");
 		equal(moment.duration(1, "milliseconds").format("h", 9), "0.000000278");
@@ -133,7 +133,7 @@ $(document).ready(function() {
 			precision: 2
 		}), "3.38 yrs");
 	});
-	
+
 	test("Using Only Settings Argument", function () {
 		equal(moment.duration(1234.55, "hours").format({
 			template: "d (days), h (hours)",
@@ -141,26 +141,26 @@ $(document).ready(function() {
 			precision: 1
 		}), "51 days, 10.6 hours");
 	});
-	
+
 	test("Zero Value Duration", function () {
 		equal(moment.duration(0, "minutes").format("m"), "0");
 		equal(moment.duration(0, "minutes").format("mm"), "00");
 		equal(moment.duration(0, "minutes").format("m", -1), "0");
 		equal(moment.duration(0, "minutes").format("mm", -1), "00");
 		equal(moment.duration(0, "minutes").format("m", 1), "0.0");
-	});	
-	
+	});
+
 	test("Default Template Function", function () {
 		equal(moment.duration(100, "milliseconds").format(), "0");
 		equal(moment.duration(100, "seconds").format(), "1:40");
 		equal(moment.duration(100, "minutes").format(), "1:40");
 		equal(moment.duration(100, "hours").format(), "4d 4h");
-		equal(moment.duration(100, "days").format(), "3m 10d");
-		equal(moment.duration(100, "weeks").format(), "23m 5d");
+		equal(moment.duration(100, "days").format(), "3m 9d");
+		equal(moment.duration(100, "weeks").format(), "22m 30d");
 		equal(moment.duration(100, "months").format(), "8y 4m");
 		equal(moment.duration(100, "years").format(), "100y");
 	});
-	
+
 	test("Custom Template Function", function () {
 		equal(moment.duration(100, "days").format(function () {
 			// map
@@ -170,24 +170,24 @@ $(document).ready(function() {
 					ret = [];
 
 				if (!array || !max) { return ret; }
-						
+
 				while (index < max) {
 					ret[index] = callback(array[index], index);
 					index += 1;
 				}
-				
+
 				return ret;
 			}
-			
+
 			var types = this.types,
 				dur = this.duration;
-				
+
 			return map(types.slice(1, -2), function (type) {
 				return ((type === "months" || type === "milliseconds") ? type[0].toUpperCase() : type[0]) + " [" + type + "]";
 			}).join(", ");
-		}), "3 months, 1 weeks, 3 days, 0 hours, 0 minutes, 0 seconds");
+		}), "3 months, 1 weeks, 2 days, 0 hours, 0 minutes, 0 seconds");
 	});
-	
+
 	test("Negative Durations", function () {
 		equal(moment.duration(-1, "years").format("y"), "-1");
 		equal(moment.duration(-1, "months").format("M"), "-1");
@@ -205,23 +205,23 @@ $(document).ready(function() {
 		equal(moment.duration(-65.667, "days").format("d", 2), "-65.67");
 		equal(moment.duration(-65.667, "days").format("d [days], h [hours]"), "-65 days, 16 hours");
 	});
-	
+
 	test("Negative Durations and Trimming", function () {
 		equal(moment.duration(-42, "seconds").format("h:mm:ss"), "-42");
 	});
-	
+
 	test("Stop Trimming with the * Character", function () {
 		equal(moment.duration(15, "seconds").format("h:*mm:ss"), "0:15");
 		equal(moment.duration(15, "seconds").format("h:*mm:ss", { forceLength: true }), "00:15");
 		equal(moment.duration(15, "seconds").format("hh:*mm:ss"), "00:15");
 		equal(moment.duration(15, "seconds").format("*h:mm:ss"), "0:00:15");
-	});	
-	
+	});
+
 	test("Decimal Separator", function () {
 		equal(moment.duration(1000, "seconds").format("h", { precision: 2 }), "0.28");
 		equal(moment.duration(1000, "seconds").format("h", { precision: 2, decimalSeparator: "," }), "0,28");
 		equal(moment.duration(1000, "seconds").format("h", { precision: 2, decimalSeparator: "abc" }), "0abc28");
 		equal(moment.duration(1000, "seconds").format("h", { precision: 2, decimalSeparator: function () { return this.template; } }), "0h28");
 		equal(moment.duration(1000, "seconds").format("h", { precision: 2, decimalSeparator: function () { return "abc"; } }), "0abc28");
-	});	
+	});
 });
